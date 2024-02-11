85600c3bb267dd2bc9669956d70cfeb7978533d4
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-11 12:33:13 +0100

    #2

--- Excercise/src/app/app.component.ts
+++ Excercise/src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   myInput = '';
+
+  getMyInput(){
+    alert(this.myInput)
+  }
 }
+
+// 1. definiowac objekt, np myInput
+// 2. w html dodac [(ngModel)] = "myInput"
+// 3. w miejscu, w ktorym ma sie pojawic ten tekst, dodac {{ myInput }} 
+
