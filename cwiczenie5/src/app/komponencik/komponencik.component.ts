16244a15098a3ccdf11fc887ef662fe1de8fbbc9
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-16 11:24:53 +0100

    cwiczenie no-tempate if else

---
+++ cwiczenie5/src/app/komponencik/komponencik.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-komponencik',
+  templateUrl: './komponencik.component.html',
+  styleUrl: './komponencik.component.css'
+})
+export class KomponencikComponent {
+  namesObject!: string;
+
+}
+
+
+// WERSJA 2
