75553467974c20a87be8ab52244c7133ef221f5d
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-20 12:25:43 +0100

    cwiczenie7

---
+++ cwiczenie6/src/app/highlight.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnInit {
+  constructor(private el: ElementRef) {}
+
+ngOnInit() {
+  this.el.nativeElement.style.color = this.getRandomColor();
+}
+
+getRandomColor(): string {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+}
---
+++ cwiczenie7/src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products = [
+    { id: 1, name: 'Product 1' },
+    { id: 2, name: 'Product 2' },
+    { id: 3, name: 'Product 3' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
