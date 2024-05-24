c3ddbaf6fc6b88a1d1e2a05638c9235773f35e5a
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-05-24 14:54:15 +0200

    weather update

--- weather/src/app/app.component.ts
+++ weather/src/app/app.component.ts
@@ -1,31 +1,41 @@
-import { Component } from "@angular/core";
-import { WeatherService } from "./weather.service";
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './weather.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   cityInput: string = '';
   weatherData: any;
   futureWeatherData: any;
+  
 
   constructor(private weatherService: WeatherService) {}
+  ngOnInit() {
 
-
+  }
+  
   getWeather() {
     const apiKey = '44c3eeabac60474782a114227240805';
-      this.weatherService.fetchData(this.cityInput, apiKey)
-        .subscribe((data: any) => {
-          this.weatherData = data;
-          console.log(this.weatherData);
-        });
-        setTimeout(() => { this.weatherService.fetchFutureData(this.cityInput, apiKey)
+    this.weatherService
+      .fetchData(this.cityInput, apiKey)
+      .subscribe((data: any) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+      });
+    setTimeout(() => {
+      this.weatherService
+        .fetchFutureData(this.cityInput, apiKey)
         .subscribe((futureData: any) => {
           this.futureWeatherData = futureData;
-          console.log(this.futureWeatherData)
-        }) }, 3000)
-    }
+          console.log(this.futureWeatherData);
+        });
+    }, 3000);
   }
-
+ 
+}
--- weather/src/app/weather.service.ts
+++ weather/src/app/weather.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
